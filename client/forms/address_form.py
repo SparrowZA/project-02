1db8ea12b5b8c79612f42779900c201c2ec3fe40
SparrowZA <geffroymarc@gmail.com>
2022-07-04 20:17:42 +0200

    Changed form to model form (Not sure what I was thinking before).

--- client/forms/address_form.py
+++ client/forms/address_form.py
@@ -2,37 +2,20 @@ from django import forms
 from client.models import Address, Client
 
 
-class AddressForm(forms.Form):
-    address_type = forms.IntegerField()
-    street = forms.CharField(
-        max_length=1024
-    )
-    street_number = forms.CharField(
-        max_length=10
-    )
-    unit_number = forms.CharField(
-        max_length=10
-    )
-    building = forms.CharField(
-        max_length=250
-    )
-    area = forms.CharField(
-        max_length=250
-    )
-    city = forms.CharField(
-        max_length=250
-    )
-    province = forms.CharField(
-        max_length=250
-    )
-    country = forms.CharField(
-        max_length=250
-    )
-    code = forms.CharField(
-        max_length=6
-    )
-
-
-# ClientInlineFormset = forms.inlineformset_factory(
-#     Client, Address, form=AddressForm, extra=2
-# )        
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = [
+            'street',
+            'street_number',
+            'unit_number',
+            'building' ,
+            'area' ,
+            'city',
+            'province',
+            'country',
+            'code' ,
+        ]
+        widgets = {
+            'address_type': forms.HiddenInput()
+        }
